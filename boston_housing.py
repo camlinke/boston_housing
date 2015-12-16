e4223fd15d998dbe3ac4582aa1d2a327d262c341
Cam Linke <cam.linke@gmail.com>
2015-12-16 09:57:04 -0700

    added a line to boston_housing to show the models best estimator. Final Version of report done

--- boston_housing.py
+++ boston_housing.py
@@ -190,9 +190,7 @@ def fit_predict_model(city_data):
     # Fit the learner to the training data
     print "Final Model: "
     print reg.fit(X, y)
-
-    print reg.get_params()
-    print reg.score(X, y)
+    print reg.best_estimator_
     
     # Use the model to predict the output of a particular sample
     x = [11.95, 0.00, 18.100, 0, 0.6590, 5.6090, 90.00, 1.385, 24, 680.0, 20.20, 332.09, 12.13]
